ec3201d9b20b122971a243b684570618ef5ac1f4
Siimy-Mo <qq1074306467@gmail.com>
2020-04-27 23:56:43 +0800

    form style finishied

--- src/views/login.vue
+++ src/views/login.vue
@@ -6,29 +6,31 @@
       <!-- 原动画平移对象 -->
 
       <div class="interface leftside working" id="login-field">
-        <div v-if="isLogin">
+        <div class="interfacebox" v-if="isLogin">
           <!-- <test/> -->
           <!-- Login Interface -->
           <transition appear appear-active-class="animated fadeIn">
-            <h2 class="titile speeddown">Login</h2>
+            <h2 class="title speeddown">Login</h2>
           </transition>
           <transition appear appear-active-class="animated fadeIn">
             <form class="speeddown">
               <div class="input-field usernameinput">
+                <i class="el-icon-user-solid" style="color:#afd2d3;"></i>
 
-                <i class="el-icon-user-solid"></i>
-                
-                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Username" tabindex ="0" />
               </div>
 
               <div class="input-field passwordinput">
-                <span>Icon</span>
+                <i class="el-icon-lock" style="color:#afd2d3;"></i>
+
                 <input type="password" placeholder="Password" />
               </div>
 
-              <div class="checkbox">
-                <input type="checkbox" />
-                <label>Remember me</label>
+              <div class="input-field checkbox">
+                <input type="checkbox" id="remember"/>
+
+                <label for="remember"></label>
+                <label id="labeltext">Remember me</label>
               </div>
 
               <button class="button submit">Login</button>
@@ -43,28 +45,26 @@
 
       <!-- 右边的框 -->
       <div class="interface rightside notworking" id="register-field">
-        <div class="working" v-if="isRegister">
+        <div class="interfacebox" v-if="isRegister">
           <!-- Login Interface -->
           <transition appear enter-active-class="animated fadeIn">
-            <h2 class="titile">Register</h2>
+            <h2 class="title speeddown">Register</h2>
           </transition>
           <transition appear enter-active-class="animated fadeIn">
             <form>
-              <div class="input-field usernameinput">
-                <span>Icon</span>
+              <div class="input-field usernameinput" onfocus="alert('hi')">
+                <i class="el-icon-user" style="color:#f4b09e;"></i>
                 <input type="text" placeholder="Username" />
               </div>
 
-              <div class="input-field emailinput">
-                <span>Icon</span>
-                <input type="email" placeholder="Email" />
-              </div>
               <div class="input-field passwordinput">
-                <span>Icon</span>
+                <i class="el-icon-lock" style="color:#f4b09e;"></i>
+
                 <input type="password" placeholder="Password" />
               </div>
               <div class="input-field passwordinput">
-                <span>Icon</span>
+                <i class="el-icon-s-check" style="color:#f4b09e;"></i>
+
                 <input type="password" placeholder="Confirm Password" />
               </div>
 
@@ -184,21 +184,130 @@ export default {
   justify-content: center;
   align-items: center;
   border-radius: 25px;
+
+  font-family: $serif;
+  color: $white;
+  font-size: 1em;
+  letter-spacing: 1.5px;
+  font-weight: 300;
 }
 .speeddown {
   animation-duration: 2s;
 }
-.input-field {
-    height: 50px;
-  margin: 5px 10%;
-  border-radius: 5px;
-  background: white;
-
-
+.title {
+  text-align: center;
+  width: 100%;
+  margin-top: 20%;
+  margin-bottom: 10%;
+  color: $white;
+  font-family: $serif;
+  letter-spacing: 5px;
 }
+.input-field {
+  display: flex;
+  align-items: center;
+  height: 45px;
+  padding: 0 10px;
+  margin: 5% 10%;
+  border-radius: 10px;
+  background: $white;
+}
+
 input {
   border: 0;
-  font-size: 16px;
-  color: white;
+  height: 60%;
+
+  font-family: $sans-serif;
+  font-size: 15px;
+  background: $white;
+  padding: 5px;
+  margin: 2px 10px;
+}
+// input[type="checkbox"] {
+  
+//     background: rgba(240, 84, 77, 1);
+//   border: 1px solid $lucy-shamrock;
+//   background: $tropical-splash;
+// }
+// input[type="checkbox"]::before {
+//   background: $tropical-splash;
+// }
+input[type=checkbox]{
+  display: none;
+}
+
+.input-field .checkbox {
+  background: red;
+}
+#remember +label{
+    display: block;
+    white-space: nowrap;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    cursor: pointer;
+    top: 2px;
+    background: $icy-morn;
+}
+ 
+#remember:checked +label::before{
+    display: block;
+    content: "\2714";
+    text-align: center;
+    font-size: 8px;
+    color: $white;
+}
+#labeltext {
+  color: $tropical-splash;
+  margin-left: 10px;
+  font-size: 10px;
+  letter-spacing: 1px;
+}
+
+.rightside input {
+  color: $first-date;
+  &::placeholder {
+    color: $first-date;
+  }
+}
+.leftside input {
+  color: $tropical-splash;
+  &::placeholder {
+    color: $tropical-splash;
+  }
+}
+
+button {
+  padding: 12px;
+  margin: 20px auto;
+  font-family: $sans-serif;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 11px;
+  border-radius: 10px;
+  outline: none;
+  display: block;
+}
+.rightside button {
+  border: 1px solid $first-date;
+  color: $peachy-dust;
+  &:hover {
+    transform: translateY(-20%);
+    background: $tropical-splash;
+    border: 1px solid $icy-morn;
+    box-shadow: 2px 0 10px $lucy-shamrock;
+    color: $white;
+  }
+}
+.leftside button {
+  border: 1px solid $icy-morn;
+  color: $tropical-splash;
+  &:hover {
+    transform: translateY(-20%);
+    background: $peachy-dust;
+    border: 1px solid $first-date;
+    box-shadow: 2px 0 10px $pale-beach;
+    color: $white;
+  }
 }
 </style>
